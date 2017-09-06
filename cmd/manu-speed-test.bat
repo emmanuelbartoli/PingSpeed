e702d96e072ee862357ec4284a80bbac6a5fbdae
Emmanuel Bartoli <emmanuel.bartoli@gmail.com>
2017-09-06 21:33:08 +0200

    Fix paths in cmd/manu-speed-test.bat

--- cmd/manu-speed-test.bat
+++ cmd/manu-speed-test.bat
@@ -1,13 +1,15 @@
 @echo off
 set PATH=C:\Users\emman\Dev\java\jdk1.8.0_111\bin;%PATH%
-set MY_HOME=C:\Users\emman\Dev\java\projects\PingSpeed\cmd
-set MY_CLASSPATH=%MY_HOME%\bin\manu-speed-test-1.0.jar;%MY_CLASSPATH%;
-set MY_CLASSPATH=%MY_CLASSPATH%;%MY_HOME%\lib\commons-logging-1.2.jar
-set MY_CLASSPATH=%MY_CLASSPATH%;%MY_HOME%\lib\log4j-1.2.17.jar
-set MY_CLASSPATH=%MY_CLASSPATH%;%MY_HOME%\lib\commons-lang3-3.6.jar
-set MY_CLASSPATH=%MY_CLASSPATH%;%MY_HOME%\lib\mysql-connector-java-5.1.42-bin.jar
-set MY_CLASSPATH=%MY_CLASSPATH%;%MY_HOME%\lib\bmartel-1.31.2.jar
+set PROJECT_HOME=C:\Users\emman\Dev\java\projects\PingSpeed\
+set LIB_HOME=C:\Users\emman\Dev\java\lib
+set CONF_HOME=C:\Users\emman\Dev\java\conf
+set MY_CLASSPATH=%PROJECT_HOME%\jar\manu-speed-test-1.0.jar;%MY_CLASSPATH%;
+set MY_CLASSPATH=%MY_CLASSPATH%;%LIB_HOME%\commons-logging-1.2.jar
+set MY_CLASSPATH=%MY_CLASSPATH%;%LIB_HOME%\log4j-1.2.17.jar
+set MY_CLASSPATH=%MY_CLASSPATH%;%LIB_HOME%\commons-lang3-3.6.jar
+set MY_CLASSPATH=%MY_CLASSPATH%;%LIB_HOME%\mysql-connector-java-5.1.42-bin.jar
+set MY_CLASSPATH=%MY_CLASSPATH%;%LIB_HOME%\bmartel-1.31.2.jar
 
-start /min javaw -cp %MY_CLASSPATH% fr.manu.speed.test.JSpeedTest %MY_HOME%\conf\speedtest.properties
+start /min javaw -cp %MY_CLASSPATH% fr.manu.speed.test.JSpeedTest %CONF_HOME%\speedtest.properties
 
 
